211e32c62a8a785c5c576b211f53f3620f1bb517
Kr1pto-psix <agent.facit.06@bk.ru>
2024-08-19 02:22:16 +0300

    Добавьте файлы проекта.

---
+++ ContentWarningEditor/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ContentWarningEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LimeGreen;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Активировать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Номер Лица: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(9, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Количестов MC:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(123, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 241);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Content Warning Edittor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ContentWarningEditor/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+using Microsoft.Win32;
+
+
+namespace ContentWarningEditor
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"..\LocalLow\Landfall Games\Content Warning\Saves\metaData.m");
+        public Form1()
+        {
+            InitializeComponent();
+            Debug.WriteLine(filePath);
+        }
+
+        void updateFace()
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                RegistryKey currentUserKey = Registry.CurrentUser;
+                RegistryKey faceTextKey = currentUserKey.OpenSubKey("Software\\Landfall Games\\Content Warning", true);
+                faceTextKey.SetValue("FaceText_h3883740665", this.textBox1.Text);
+                faceTextKey.Close();
+            }
+        }
+
+        void updateMeatCoins()
+        {
+            string json = File.ReadAllText(filePath);
+            // Находим индекс "MetaCoins"
+            int index = json.IndexOf("\"MetaCoins\":");
+
+            if (index == -1)
+                return ; // Если не найдено, возвращаем оригинальный JSON
+
+            // Находим конец значения
+            int startIndex = index + "\"MetaCoins\":".Length;
+            int endIndex = json.IndexOf(",", startIndex);
+
+            // Если запятая не найдена, ищем конец объекта
+            if (endIndex == -1)
+            {
+                endIndex = json.IndexOf("}", startIndex);
+            }
+
+            // Формируем новое значение
+            string newMetaCoins = this.textBox2.Text;
+
+            // Заменяем старое значение на новое
+            json = json.Substring(0, startIndex) + newMetaCoins + json.Substring(endIndex);
+            if (textBox2.Text.Length > 0)
+            {
+                File.WriteAllText(filePath, json);
+
+            }
+            
+
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Process.GetProcessesByName("Content Warning").Any())
+            {
+                if (MessageBox.Show("Игра не должна быть запущена, Закрыть игру?", "Ошибка", MessageBoxButtons.YesNo) ==
+                    DialogResult.Yes)
+                {
+                    foreach (Process proc in Process.GetProcessesByName("Content Warning"))
+                    {
+                        proc.Kill();
+                    }
+                    updateMeatCoins();
+                    updateFace();
+                    MessageBox.Show("Изменения внесены успешно");
+                    return;
+                }
+                MessageBox.Show("Ошибка внесения изменений");
+                
+            }
+            else
+            {
+                updateMeatCoins();
+                updateFace();
+                MessageBox.Show("Изменения внесены успешно");
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Проверяем, является ли введённый символ цифрой
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                // Запрещаем ввод символов, кроме цифр
+                e.Handled = true;
+            }
+        }
+    }
+}
